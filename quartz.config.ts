faeb8b2858eff10fcc71dafacd4887e6c79d7789
David <davidkizajr@protonmail.com>
2024-05-27 21:07:33 +0200

    Quartz sync: May 27, 2024, 9:07 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "redeemed-flower.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
